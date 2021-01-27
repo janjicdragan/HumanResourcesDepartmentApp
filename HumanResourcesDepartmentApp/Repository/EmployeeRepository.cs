5bdea2bc97bc23a04b41250cdce76c59379af8d1
Dragan Janjic <janjic.drgn@gmail.com>
2021-01-27 16:22:19 +0100

    Created repository folder and classes, implemented logic

---
+++ HumanResourcesDepartmentApp/Repository/EmployeeRepository.cs
@@ -0,0 +1,115 @@
+﻿using HumanResourcesDepartmentApp.Interfaces;
+using HumanResourcesDepartmentApp.Models;
+using HumanResourcesDepartmentApp.Models.FilterObjects;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace HumanResourcesDepartmentApp.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository, IDisposable
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                {
+                    db.Dispose();
+                    db = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerable<Employee> ReadAll()
+        {
+            return db.Employees.Include(e => e.OrganizationalUnit);
+        }
+
+        public Employee ReadById(int id)
+        {
+            return db.Employees.Include(e => e.OrganizationalUnit).FirstOrDefault(e => e.Id == id);
+        }
+
+        public void Add(Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+        }
+
+        public void Update(Employee employee)
+        {
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(Employee employee)
+        {
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<OrganizationalUnitNumberOfEmployeesDTO> ReadOrganizationalUnitsWithNumberOfEmployees()
+        {
+            return db.Employees
+                .Include(e => e.OrganizationalUnit)
+                .GroupBy(e => e.OrganizationalUnit, e => e.OrganizationalUnitId,
+                (organizationalUnit, numberOfEmployees) => new OrganizationalUnitNumberOfEmployeesDTO()
+                {
+                    Id = organizationalUnit.Id,
+                    OrganizationalUnit = organizationalUnit.Name,
+                    NumberOfEmployees = numberOfEmployees.Count()
+                }).OrderByDescending(o => o.NumberOfEmployees);
+        }
+
+        public IEnumerable<OrganizationalUnitAverageSalaryDTO> ReadOrganizationalUnitsWithAverageSalary(OverLimitSalaryFilter overLimitSalaryFilter)
+        {
+            return db.Employees
+                .Include(e => e.OrganizationalUnit)
+                .GroupBy(e => e.OrganizationalUnit, e => e.Salary,
+                (organizationalUnit, salary) => new OrganizationalUnitAverageSalaryDTO()
+                {
+                    Id = organizationalUnit.Id,
+                    OrganizationalUnit = organizationalUnit.Name,
+                    AverageSalary = salary.Average()
+                }).Where(o => o.AverageSalary > overLimitSalaryFilter.Limit)
+                .OrderBy(o => o.AverageSalary);
+
+        }
+
+        public IEnumerable<Employee> ReadEmployeesByBirthYear(int birthYear)
+        {
+            return db.Employees
+                .Include(e => e.OrganizationalUnit)
+                .Where(e => e.BirthYear > birthYear)
+                .OrderBy(e => e.BirthYear);
+        }
+
+        public IEnumerable<Employee> ReadEmployeesByGivenSalary(BetweenLimitsSalaryFilter betweenLimitsSalaryFilter)
+        {
+            return db.Employees
+                .Include(e => e.OrganizationalUnit)
+                .Where(e => e.Salary >= betweenLimitsSalaryFilter.MinSalary && e.Salary <= betweenLimitsSalaryFilter.MaxSalary)
+                .OrderByDescending(e => e.Salary);
+        }
+    }
+}
---
+++ HumanResourcesDepartmentApp/Repository/OrganizationalUnitRepository.cs
@@ -0,0 +1,54 @@
+﻿using HumanResourcesDepartmentApp.Interfaces;
+using HumanResourcesDepartmentApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace HumanResourcesDepartmentApp.Repository
+{
+    public class OrganizationalUnitRepository : IOrganizationalUnitRepository, IDisposable
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                {
+                    db.Dispose();
+                    db = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerable<OrganizationalUnit> ReadAll()
+        {
+            return db.OrganizationalUnits;
+        }
+
+        public OrganizationalUnit ReadById(int id)
+        {
+            return db.OrganizationalUnits.FirstOrDefault(o => o.Id == id);
+        }
+
+        public IEnumerable<OrganizationalUnit> ReadTradition()
+        {
+            List<OrganizationalUnit> result = new List<OrganizationalUnit>();
+            IEnumerable<OrganizationalUnit> data = db.OrganizationalUnits.OrderBy(o => o.YearOfFoundation);
+
+            result.Add(data.FirstOrDefault());
+            result.Add(data.LastOrDefault());
+
+            return result.AsEnumerable();
+        }
+    }
+}
